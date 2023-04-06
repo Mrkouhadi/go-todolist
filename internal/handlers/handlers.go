4f18b7d1731788ce8309e5a029350eb93d56a821
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-04-07 01:10:42 +0800

    feat: added jsonresponse handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,7 +12,9 @@ import (
 	"github.com/mrkouhadi/go-todolist/internal/rendertemplates"
 )
 
-/******************************************** REPOSITORY ****************************************/
+/**************************************************************************
+// REPOSITORY
+***************************************************************************/
 
 type Repository struct {
 	AppConfig *config.Appconfiguration
@@ -24,29 +28,86 @@ func NewRepository(AC *config.Appconfiguration) *Repository {
 	}
 }
 
-/******************************************** handlers ****************************************/
+/*
+***********************************************************************************
+// handlers
+***********************************************************************************
+*/
 func NewHandlers(R *Repository) {
 	Repo = R
 }
 
-// HOME
+// ******************* HOME
 func (repo *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	// repo.AppConfig.Session.Put(r.Context(), "greet", "Hello the world of golang devs")
 	rendertemplates.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
-// TODOS
-func (repo *Repository) AllTodos(w http.ResponseWriter, r *http.Request) {
-	// greet := repo.AppConfig.Session.GetString(r.Context(), "greet")
-	rendertemplates.RenderTemplate(w, r, "alltodos.page.tmpl", &models.TemplateData{})
+// ******************* TODOS
+// GET json
+type jsonRes struct {
+	OK      bool          `json:"ok"`
+	MESSAGE []models.TODO `json:"message"`
 }
 
-// SEARCH
+func (repo *Repository) AllTodos(w http.ResponseWriter, r *http.Request) {
+	// greet := repo.AppConfig.Session.GetString(r.Context(), "greet")
+	resp := jsonRes{
+		OK: true,
+		MESSAGE: []models.TODO{
+			{
+				Title:       "hello gophers",
+				Email:       "bryan@kouhadi.com",
+				Content:     "this is  the content of my todo ",
+				IsImportant: true,
+				IsDone:      false,
+				Time:        time.Now(),
+			},
+			{
+				Title:       "hello gophers 2",
+				Email:       "bryan@kouhadi2.com",
+				Content:     "this is  the content of my todo 2",
+				IsImportant: false,
+				IsDone:      true,
+				Time:        time.Now(),
+			},
+		},
+	}
+	out, err := json.MarshalIndent(resp, "", "    ")
+	if err != nil {
+		log.Println(err)
+	}
+	// specify the content-type
+	w.Header().Set("Content-Type", "application/json")
+	// send data as json to the client
+	w.Write(out)
+	// rendertemplates.RenderTemplate(w, r, "alltodos.page.tmpl", &models.TemplateData{})
+}
+
+// ******************* SEARCH
+// GET
 func (repo *Repository) SearchTodos(w http.ResponseWriter, r *http.Request) {
 	rendertemplates.RenderTemplate(w, r, "search.page.tmpl", &models.TemplateData{})
 }
 
-// add new todo
+// POST
+func (repo *Repository) PostSearchTodos(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	title := r.Form.Get("title")
+	done := convertStrToBool(r.Form.Get("done"))
+	important := convertStrToBool(r.Form.Get("important"))
+
+	log.Println(title, done, important)
+	w.Write([]byte(fmt.Sprintf("title: %s", title)))
+
+	// rendertemplates.RenderTemplate(w, r, "search.page.tmpl", &models.TemplateData{})
+}
+
+// ****************** ADD NEW TODOS
 //
 //	GET
 func (repo *Repository) AddNewTodo(w http.ResponseWriter, r *http.Request) {
@@ -73,12 +134,14 @@ func (repo *Repository) PostNewTodo(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]interface{})
 	data["todo"] = newTodo
 
-	log.Println(newTodo)
 	rendertemplates.RenderTemplate(w, r, "addnewtodo.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
 }
 
+/********************************************************************************
+//  HELPERS
+**********************************************************************************/
 // convertStrToBool helps us convert value of checkbox from "on"/"off" to true/false
 func convertStrToBool(s string) bool {
 	return s == "on"
