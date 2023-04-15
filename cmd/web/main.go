e3517c4f40cd9e2aeca8ef42ee61d73f025f9b4d
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-04-16 02:48:18 +0800

    feat: added testing set up

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"log"
 	"net/http"
 	"time"
@@ -8,6 +9,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/mrkouhadi/go-todolist/internal/config"
 	"github.com/mrkouhadi/go-todolist/internal/handlers"
+	"github.com/mrkouhadi/go-todolist/internal/models"
 	"github.com/mrkouhadi/go-todolist/internal/rendertemplates"
 )
 
@@ -19,10 +21,25 @@ var appConfig config.Appconfiguration
 var todoSessionManager *scs.SessionManager
 
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// run the server
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: Routes(&appConfig),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// turn production mode or not yet
 	appConfig.InProduction = false
 
 	// set up the session
+	gob.Register(models.TODO{})
 	todoSessionManager = scs.New()
 	todoSessionManager.Lifetime = 24 * time.Hour
 	todoSessionManager.Cookie.Persist = true
@@ -34,6 +51,7 @@ func main() {
 	tmplCache, err := rendertemplates.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create templates cache !!!")
+		return err
 	}
 	appConfig.TemplateCache = tmplCache
 	appConfig.UseCache = false
@@ -43,11 +61,5 @@ func main() {
 	handlers.NewHandlers(newRepo)
 
 	rendertemplates.NewTemplates(&appConfig)
-	// run the server
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: Routes(&appConfig),
-	}
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
